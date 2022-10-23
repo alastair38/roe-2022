b658dc2b2c3d9390f70ace7a87e6fe1a14315dcb
alastair38 <alastair@alastaircox.com>
2022-10-23 21:07:30 +0100

    margin-right for brand wrapper at lg screen sizes

--- header.php
+++ header.php
@@ -26,7 +26,7 @@
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'blockhaus' ); ?></a>
 
 	<header id="masthead" class="bg-primary-default fixed top-0 left-0 right-0 px-2 lg:flex-row lg:relative flex justify-between items-center border-b-4 border-offset">
-		<div class="flex justify-center items-center gap-0 lg:gap-2 w-full lg:w-auto">
+		<div class="flex justify-center items-center gap-0 lg:mr-6 lg:gap-2 w-full lg:w-auto">
 
    <img class="h-12" src="/wp-content/themes/roe-2022/assets/images/logo/logo.png" alt="Graphic showing a tree-lined city skyline">
 				<span class="text-base mx-auto text-center lg:text-left lg:text-xl font-black uppercase"><a class="block w-max" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></span>
