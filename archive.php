0072ef00318e5d403fc6df74d898ad517cb60c67
alastair38 <alastair@alastaircox.com>
2022-10-23 20:56:43 +0100

    index / archive.php to grid-cols-2 for md screens

--- archive.php
+++ archive.php
@@ -41,7 +41,7 @@ $post_type_obj = get_post_type_object( $post_type );
 
 			<?php echo blockhaus_custom_form("Search " . $post_type_obj->labels->name . " ..."); ?>
 
-			<div class="grid w-full grid-cols-1 md:grid-cols-3 gap-6">
+			<div class="grid w-full grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
 				
 			<?php $description = get_field(get_post_type() . '_page_description', "options");
 
--- index.php
+++ index.php
@@ -50,7 +50,7 @@ get_header();
 <?php echo blockhaus_custom_form("Search blog posts ..."); ?>
 
 
-<div class="grid w-full grid-cols-1 md:grid-cols-3 gap-6">
+<div class="grid w-full grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
 
 				<?php
 			endif;
